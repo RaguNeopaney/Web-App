a5db48abeb86651f2aec9fd60f96e29918e7b022
Ragu Neopaney <raguneopaney@gmail.com>
2023-02-19 11:01:37 -0500

    Inital commit for the project set-up

---
+++ Source Code/Voltorb Flip Web App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Voltorb_Flip_Web_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
